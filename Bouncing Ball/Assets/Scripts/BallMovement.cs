dd8c82733d8131f75a9e16e15f01443fbddfa73e
TerraOS-creator <hanasakuya32@gmail.com>
2021-10-09 11:53:19 +0700

    Problem 4

--- Bouncing Ball/Assets/Scripts/BallMovement.cs
+++ Bouncing Ball/Assets/Scripts/BallMovement.cs
@@ -4,31 +4,42 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+    public KeyCode rightButton = KeyCode.D;
+    public KeyCode leftButton = KeyCode.A;
+    public float speed = 10.0f;
     private Rigidbody2D rigidBody2D; 
-    public float xInitialForce;
-    public float yInitialForce;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-        PushBall();
-    }
-    void PushBall()
-    {
-        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
-        float randomDirection = Random.Range(0, 2);
-        if (randomDirection < 1.0f)
-        {
-            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
-        }
-        else
-        {
-            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
-        }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 velocity = rigidBody2D.velocity;
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }  
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else if (Input.GetKey(leftButton))
+        {
+            velocity.x = -speed;
+        }
+        else if (Input.GetKey(rightButton))
+        {
+            velocity.x = speed;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        rigidBody2D.velocity = velocity;
     }
 }
