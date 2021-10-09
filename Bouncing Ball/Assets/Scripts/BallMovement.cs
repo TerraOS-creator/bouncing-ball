911421ee8583fdf6a481ceb8ec07666e30169480
TerraOS-creator <hanasakuya32@gmail.com>
2021-10-09 11:18:20 +0700

    Problem 2

---
+++ Bouncing Ball/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D; 
+    public float xInitialForce;
+    public float yInitialForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        PushBall();
+    }
+    void PushBall()
+    {
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+        float randomDirection = Random.Range(0, 2);
+        if (randomDirection < 1.0f)
+        {
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
