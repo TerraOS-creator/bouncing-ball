13cdf8bd605306496d1d93b02a42f9b7129d9e56
TerraOS-creator <hanasakuya32@gmail.com>
2021-10-30 23:41:56 +0700

    final

---
+++ Bouncing Ball/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int counter=0;
+    int score;
+    public Spawner sp;
+    public ScoreManage sm;
+    public CircleCollider2D col;
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        string tag = col.gameObject.tag;
+        if (tag == "Enemy") { 
+        Destroy(col.gameObject);
+        counter++;
+            sm.currentScore = TempScore();
+            sp.counter--;
+
+            }
+
+    }
+    
+    public int TempScore()
+    {
+        
+        score = counter  ;
+        return score;
+    }
+
+    
+}
---
+++ Bouncing Ball/Assets/Scripts/Spawner.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameFlowManager gf;
+    private Rigidbody2D rb;
+    public Transform[] Spawnpoints;
+    public float timeRemaining = 10;
+    public int counter = 0;
+    [SerializeField]
+    private GameObject enemy;
+    public float secondsBetweenSpawn = 3;
+    public float elapsedTime;
+    private int nextUpdate = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+
+        // If the next update is reached
+        if (Time.time >= nextUpdate)
+        {
+            Debug.Log(Time.time + ">=" + nextUpdate);
+            // Change the next update (current second+1)
+            nextUpdate = Mathf.FloorToInt(Time.time) + 1;
+            // Call your fonction
+            UpdateEverySecond();
+        }
+
+    }
+    void UpdateEverySecond()
+    {
+
+        if ( timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            elapsedTime++;
+            if(elapsedTime > secondsBetweenSpawn)
+            {
+               elapsedTime = 0;
+            Debug.Log(true);
+            Spawn();
+            }
+                
+            
+        }
+        else if (timeRemaining == 0)
+        {
+            gf.GameOver();
+        }
+        elapsedTime += Time.deltaTime;
+
+    }
+    public void Spawn()
+    {
+        bool obstaclespawn = false;
+
+        if (counter < 7)
+        {
+            while (!obstaclespawn)
+            {
+
+                Vector3 hexagonplace = new Vector3(Random.Range(-7f, 7f), Random.Range(-3f, 3f), 0f);
+                Instantiate(enemy, hexagonplace, Quaternion.identity);
+                obstaclespawn = true;
+                counter++;
+
+            }
+        }
+
+    }
+
+}
+
+
